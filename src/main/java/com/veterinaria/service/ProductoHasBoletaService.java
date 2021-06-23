22c11d8d015939f67e262d857f4e94c477b33d5b
Jcampos43 <jc288753@gmail.com>
2021-06-23 03:07:11 -0500

    Detalle Boleta

---
+++ src/main/java/com/veterinaria/service/ProductoHasBoletaService.java
@@ -0,0 +1,11 @@
+package com.veterinaria.service;
+
+import java.util.List;
+
+import com.veterinaria.entity.*;
+
+public interface ProductoHasBoletaService {
+	
+	public List<ProductoHasBoleta> detalleBoleta(int num_boleta);
+	
+}
